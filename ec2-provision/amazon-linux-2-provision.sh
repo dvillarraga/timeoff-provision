9b213e2f45901ece8fefa87633b49c5519b64043
Oscar Daniel Villarraga Castro <dvillarraga@gmail.com>
2020-08-12 10:48:17 -0500

    Update amazon-linux-2-provision.sh

--- ec2-provision/amazon-linux-2-provision.sh
+++ ec2-provision/amazon-linux-2-provision.sh
@@ -10,6 +10,37 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 ### Installing Packages
 sudo yum install -y git gcc-c++ make ruby wget
+### Installing nginx proxy server
+sudo amazon-linux-extras install -y nginx1
+sudo mkdir /etc/nginx/ssl
+sudo openssl req -new -newkey rsa:2048 -nodes -days 365 -x509 \
+    -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt \
+    -subj "/C=US/ST=Denial/L=GFwYvGFSWfm4rNMg/O=Dis/CN=amazonaws.com"
+sudo bash -c "cat > /etc/nginx/conf.d/timeoffapp.conf" << EOF
+server {
+    listen 80;
+    server_name amazonaws.com;
+
+    location / {
+        proxy_set_header   X-Forwarded-For \$remote_addr;
+        proxy_set_header   Host \$http_host;
+        proxy_pass         http://localhost:3000;
+    }
+}
+server {
+    listen 443 ssl;
+    server_name amazonaws.com;
+    ssl_certificate /etc/nginx/ssl/nginx.crt;
+    ssl_certificate_key /etc/nginx/ssl/nginx.key;
+
+    location / {
+        proxy_set_header   X-Forwarded-For \$remote_addr;
+        proxy_set_header   Host \$http_host;
+        proxy_pass         http://localhost:3000;
+    }
+}
+EOF
+sudo systemctl start nginx
 # Installing CodeDeploy Agent
 cd /home/ec2-user
 wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
